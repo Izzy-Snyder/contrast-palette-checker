6a0725413e1f711da7a8ffffdd096cf38396d9e4
Izzy-Snyder <izzysnyder2001@gmail.com>
2025-08-14 22:25:47 -0400

    version 2 - outputs a text file for a p5 file

--- contrast.py
+++ contrast.py
@@ -1,12 +1,12 @@
-#todo
-#eventually: move everything to p5.js OR use a python drawing module OR output a file that p5 can use as input(probably easies)
-#add in labels for the pages
-#make directory with file path
-#account for opacity
-
-import math
 from functools import cmp_to_key
 
+lightbg = "lighter" #color name of light theme bg
+darkbg = "darker" #color name of dark theme bg
+paletteFile = "_variables.scss" #file name for color palette
+randomColors = True #use unnamed colors that appear in themes
+
+
+#hex digit to int converts hex digit to base 10 int
 def hexDigitToInt(digit):
     if digit.isdigit():
         return int(digit)
@@ -23,13 +23,14 @@ def hexDigitToInt(digit):
     if (digit.lower() == 'f'):
         return 15
 
+#converts hex to RGB
 def hexToRGB(hex):
     num1 = hexDigitToInt(hex[0])*16+hexDigitToInt(hex[1])
     num2 = hexDigitToInt(hex[2])*16+hexDigitToInt(hex[3])
     num3 = hexDigitToInt(hex[4])*16+hexDigitToInt(hex[5])
     return ([num1,num2,num3])
 
-
+#converts RGB to luminance
 def RGBToLumRGB (n):
     n = n/255
     if n <= 0.03928:
@@ -37,29 +38,36 @@ def RGBToLumRGB (n):
     else:
         return ((n+0.055)/1.055)**2.4
 
+#gets relative luminance using formula found online
 def contrast (hex1, hex2):
 
     c1 = hexToRGB(hex1.strip('#'))
     c2 = hexToRGB(hex2.strip('#'))
 
+    #get luminance of RGB 1
     R1=RGBToLumRGB(c1[0])
     G1=RGBToLumRGB(c1[1])
     B1=RGBToLumRGB(c1[2])
 
+    #calculate overall luminance 1
     L1 = 0.2126 * R1 + 0.7152 * G1 + 0.0722 * B1
 
+    #get luminance of RGB 2
     R2=RGBToLumRGB(c2[0])
     G2=RGBToLumRGB(c2[1])
     B2=RGBToLumRGB(c2[2])
-
+    
+    #calculate overall luminance 2
     L2 = 0.2126 * R2 + 0.7152 * G2 + 0.0722 * B2
 
+    #find lighter and darkrer luminance
     lighter = max(L1,L2)
     darker = min(L1,L2)
 
+    #return ratio between them (the contrast)
     return round((lighter+0.05)/(darker+0.05),1)
     
-
+#comparitor for sorting color items by contrast
 def contrastSort(a, b):
     con1 = float(a[3].partition(":")[0])
     con2 = float(b[3].partition(":")[0])
@@ -70,20 +78,23 @@ def contrastSort(a, b):
     else:
         return 1
 
+#takes in a dictionary of colors and a list of background colors. 
+# Outputs a dictionary with the name of the colors as the key and information on the colors as the values.
 def colorPaletteCheck(colors,bgcolors):
     
     contrasts = {}
     
     colorDict = colors
+    
     colors = list(colors.keys())
+    
     compare_key = cmp_to_key(contrastSort)
+    
+    #iterate through bg colors and find contrast with each foreground color
+    #add that information to a contrast set object and return those objects in a dictionary
     for color1 in bgcolors:
         smallText = []
         largeTextUI = []
-        '''
-        print("background: " + colorDict[color1] + " (" + color1 + ")")
-        print()
-        '''
         for color2 in colors:
             con = contrast(color1,color2)
             s = [color2, colorDict[color2],  str(hexToRGB(color2.strip("#"))), (str(con) + ":1")]
@@ -95,133 +106,64 @@ def colorPaletteCheck(colors,bgcolors):
         largeTextUI = sorted(largeTextUI, key=compare_key)
         newContrastSet = ContrastSet(smallText,largeTextUI,colorDict[color1],color1)
         contrasts[newContrastSet.name] = newContrastSet
-        '''
-        print("Small text:")
-        for item in smallText:
-            print(item)
-        print()
-        print("Large Text or UI:")
-        for item in largeTextUI:
-            print(item)
         
-        print()
-        print()
-        '''
     return contrasts
 
+#writes to a text file to be used as input in the p5.js program
+#contrasts: dictionary of contrasts, 
+# bg1/2: color of the main and secondary bg color, 
+# Theme: name of theme, 
+# outfile: name of output file. 
+#small: boolean for whether to use large text or small text contrast sets
+#sub: boolean for whether it's a sub-palette or not (text on button colors)
 def makeTextFile(contrasts, bg1, bg2, theme, outFile,small, sub=False):
     c = contrasts[bg1]
-
     
-    if sub:
-        outFile.write("## UI component color: "  + contrasts[bg1].name + " (" + contrasts[bg1].hex + ")\n")
-    else:
-        outFile.write("# Theme: "  + theme + "\n\n")
-        outFile.write("## UI or large text colors: \n")
+    #get contrast of the UI element with the background
+    buttonContrast = ""
+    for l in contrasts[bg2].large:
+        if(l[1]) == bg1:
+            buttonContrast = (l[3])
 
+    #write starting line for the given foreground/background
+    if sub:
+        outFile.write("start "  + theme + " " + contrasts[bg2].name + " " + contrasts[bg2].hex + " " + contrasts[bg2].RGBstr + " sub" + "\n")
+        outFile.write(contrasts[bg1].name + " " + contrasts[bg1].hex + " "  + contrasts[bg1].RGBstr + " " + buttonContrast +"\n")
+    else:
+        outFile.write("start "  + theme + " " +  contrasts[bg2].name + " " + contrasts[bg2].hex  + " " + contrasts[bg2].RGBstr + " " + "\n")
+
+    #determine whether to use small or large text contrast set
     if small:
         colorList = c.small
     else:
         colorList = c.large
 
+    #write out the information on each color
     for s in colorList:
-        outFile.write( "* " + s[1] + " (" + str(s[0]) + ")" + str(s[2]) +  " contrast: " + str(s[3])+ "\n")
-
-    outFile.write("\n")
+        outFile.write(s[1] + " " + str(s[0]) + " " +  str(s[2]).translate({ord(i): None for i in '[],'}) + " " + str(s[3])+ "\n")
     
-
-def makeP5(contrasts, bg1,bg2,rectSize,rowLength,padding,outFile,small, sub=False):
-    c = contrasts[bg1]
-    w = round(rectSize*rowLength+padding*(rowLength+1))
-
+    #write final line
+    outFile.write("end\n\n")
     
-    
-    backgroundRGB = str(contrasts[bg2].RGB)
-    rectRGB = str(contrasts[bg1].RGB)
-    
-
-    outFile.write("bgcolor = "  + backgroundRGB + "\n")
-    if sub:
-        outFile.write("rectColor = "  + rectRGB + "\n")
-
-    if small:
-        colorList = c.small
-    else:
-        colorList = c.large
-
-    for s in colorList:
-        outFile.write( s[1].replace("-","") + " = " + str(s[2]) + ";\n")
-    
-    h = round(rectSize*(math.ceil(len(colorList)/rowLength))+padding*((math.ceil(len(colorList)/rowLength))+1))+padding
-    if sub:
-        h+=padding*1.25
-
-    outFile.write("\nfunction setup() {")
-    outFile.write("\n\tcreateCanvas ("+str(w)+", "+str(h)+");")
-    outFile.write("\n\tbackground (bgcolor);")
-    outFile.write("\n\ttextAlign(CENTER);")
-    outFile.write("\n\ttextSize("+str(12*rectSize/100)+");")
-   
-    x = 0
-    y = 0
-    for s in (colorList):
-        n = (s[1].replace("-",""))
-        if sub:
-            outFile.write("\n\tfill (rectColor);")
-        else:
-            outFile.write("\n\tfill ("+n+");")
-        outFile.write("\n\trect "+ str((x*(rectSize+padding)+padding, y*(rectSize+padding)+padding, rectSize))+";")
-        if sub:
-            outFile.write("\n\tfill ("+n+");")
-        else:
-            outFile.write("\n\tfill (bgcolor);")
-        outFile.write("\n\ttext"+str((str(s[1]),x*(rectSize+padding)+padding+rectSize/2, y*(rectSize+padding)+padding+rectSize/3))+"")
-        outFile.write("\n\ttext"+str(((s[0]),x*(rectSize+padding)+padding+rectSize/2, y*(rectSize+padding)+padding+rectSize/2))+"")
-        outFile.write("\n\ttext"+str((s[3],x*(rectSize+padding)+padding+rectSize/2, y*(rectSize+padding)+padding+2*rectSize/3))+"")
-
-        x+=1
-        if (x >= rowLength):
-            y+=1
-            x = 0
-
-    if sub:
-        outFile.write("\n\tfill (rectColor);") 
-    else:
-        outFile.write("\n\tfill (" + colorList[0][1].replace("-","")+ ");") 
-
-
-        
-    outFile.write("\n\ttextAlign(LEFT);")
-    outFile.write("\n\ttextSize("+str(16*rectSize/150)+");")
-    if sub:
-        outFile.write("\n\ttext"+str(("background: "+ bg2 + ", " + str(contrasts[bg2].hex), padding, h-padding/2))+"") #find way to add hex later
-        outFile.write("\n\ttext"+str(("UI element color: "+ str(c.name) + ", " + str(c.hex),padding, h-1.75*padding))+"")
-    else:
-        outFile.write("\n\ttext"+str(("background: "+ str(c.name) + ", " + str(c.hex),padding, h-padding/2))+"")
-    outFile.write("\n\tsaveCanvas('" + outFile.name[:-3]+"')")
-    outFile.write("\n}\n\n")
-
-
+#main function reads in palette file, creates data structure of contrasts, outputs to a file
+#assumes an input file of scss variables where hex codes are indicated with a # and variables are indicated with a $
 def main():
-    #print(contrast("F0F0F0","F0F0F0"))
-    #print(contrast("#ED225D","#F5DC22"))
-    lightbg = "lighter"
-    darkbg = "darker"
-    rectSize = 100
-    rowLength = 5
-    padding = rectSize/10
 
-
-    f = open("palette.txt","r")
+    #open the file, initialize a dictonary to store the variable names and hex values
+    f = open(paletteFile,"r")
     file = (f.readlines())
     palette = {}
-    paletteInv = {}
+
+    #iterate through file and add characters to color names and color hex values
     for line in file:
         colorName = ""
         colorHex = ""
-        if line[0] == "$":
+        #always check if line contains hex. 
+        # If random colors is set to true, no need for line to start with a $ for a variable
+        if "#" in line and (line[0] == "$" or (randomColors)):
             name = True
             color = False
+            #add things to either the color name or the color hex depending on surrounding characters
             for char in line:
                 if (char != ":" and name):
                     colorName += char
@@ -232,31 +174,24 @@ def main():
                 if (char == ":"):
                     name = False
                     color = True
+            #if it's a color like '#313' then that actually means '#313313', so add on a copy of the start
             if (len(colorHex)<7):
-                colorHex = colorHex + colorHex[2:]   
-            #print(colorHex)
-            palette [colorHex.strip()] = colorName.strip("$")
-            paletteInv [colorName.strip("$")] = colorHex.strip()
+                colorHex = colorHex + colorHex[2:] 
+            #take out extra characters from the name and hex
+            colorName = colorName.strip("$  / ")
+            colorHex = colorHex.strip(",\n ").upper()
+            
+            #this excludes opacity colors (I have not implemented alpha compositing)
+            if (len(colorHex)<=7):  
+                palette [colorHex] = colorName
 
-
+    #generate a reference dictionary of every color's contrast with every other color
     contrasts = colorPaletteCheck(palette,palette)
-    #print(contrasts)
 
+    #opens an output file
+    outFileText = open("contrasts.txt", "w")
 
-    outFileTextLight = open("pallete_contrasts_light.md", "w")
-    outFileTextDark = open("pallete_contrasts_dark.md", "w")
-
-
-    outFileLight = open("contrast_combinations_light_large_UI.js", "w")
-
-    makeP5(contrasts,lightbg,lightbg,rectSize,rowLength,padding, outFileLight,True)
-
-    outFileDark = open("contrast_combinations_dark_large_UI.js", "w")
-
-    makeP5(contrasts,darkbg,darkbg,rectSize,rowLength,padding, outFileDark,True)
-
-
-    #print(contrasts['p5js-pink'].large)
+    #breaks contrast into two lists for light and dark bg contrasts
     lightContrasts = []
     for c in contrasts[lightbg].large:
         lightContrasts.append(c[1])
@@ -265,50 +200,20 @@ def main():
     for c in contrasts[darkbg].large:
         darkContrasts.append(c[1])
 
-    outPutList = [outFileDark.name,outFileLight.name]
+    #add light and dark full palette contrasts to the output file
+    makeTextFile(contrasts,lightbg, lightbg, "light", outFileText, False, False)     
+    makeTextFile(contrasts,darkbg, darkbg, "dark", outFileText, False, False)     
     
-    makeTextFile(contrasts,lightbg, lightbg, "light", outFileTextLight, False, False)     
-    makeTextFile(contrasts,darkbg, darkbg, "dark", outFileTextDark, False, False)     
-    
+    #add all the sub-palette contrasts to the output file
     for subColor in contrasts.keys():
         if subColor in lightContrasts:
-            outFileSubLight = open("contrast_combinations_light_subcolor_" + subColor + ".js", "w")
-            outPutList.append(outFileSubLight.name)
-            makeP5(contrasts,subColor, lightbg,rectSize,rowLength,padding, outFileSubLight,False, True)
-            makeTextFile(contrasts,subColor,lightbg, "light", outFileTextLight, False, True)     
+            makeTextFile(contrasts,subColor,lightbg, "light", outFileText, False, True)     
 
         if subColor in darkContrasts:
-            outFileSubDark = open("contrast_combinations_dark_subcolor_" + subColor + ".js", "w")
-            outPutList.append(outFileSubDark.name)
-            makeP5(contrasts,subColor, darkbg,rectSize,rowLength,padding, outFileSubDark,False, True)
-            makeTextFile(contrasts,subColor,darkbg, "dark/contrast", outFileTextDark, False, True)     
+            makeTextFile(contrasts,subColor,darkbg, "dark/contrast", outFileText, False, True)     
 
 
-    indexOld = open("index-reference.html","r")
-    indexNew = open("index.html", "w")
-
-    script = False
-    for line in indexOld:
-        
-        if ('<script src="sketch.js"></script>' in line):
-            script = True
-        else:
-            indexNew.write(line)
-        if script:
-           for o in outPutList:
-            indexNew.write('\t\t<script src="'+o+'"></script>\n') 
-            script = False
-    
-
-    '''
-    for key in contrasts:
-        c = contrasts[key]
-        outFile.write("Background: ")
-        outFile.write(c.name + "\n")
-        outFile.write("Small Text: ")
-        outFile.write(str(c.small))
-    '''
-    #colorPaletteCheck(["000000","FFFFFF","F0F0F0"])
+#define a contrast set class   
 class ContrastSet():
     def __init__(self, small, large, name, hex):
         self.small = small
@@ -316,8 +221,9 @@ class ContrastSet():
         self.name = name
         self.hex = hex
         self.RGB = str(hexToRGB(hex.strip("#")))
+        self.RGBstr = self.RGB.translate({ord(i): None for i in '[],'})
 
 
-
+#run main
 main()
     
